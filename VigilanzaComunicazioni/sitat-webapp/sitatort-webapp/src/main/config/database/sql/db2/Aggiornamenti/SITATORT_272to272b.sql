7f7968247ac0acf0a907db7419b20d92e67d7d35
Alvise Gorinati <alvise.gorinati@maggioli.it>
2022-01-27 09:31:03 +0100

    Pulizia sitat-webapp

--- VigilanzaComunicazioni/sitat-webapp/sitatort-webapp/src/main/config/database/sql/db2/Aggiornamenti/SITATORT_272to272b.sql
+++
@@ -1,9 +0,0 @@
---------------------------------
--- AGGIORNAMENTO SITAT/VIGILANZA
--- Database: DB2
-------------------------------
-
-------------------------------
--- SITATORT 2.7.2.b
-------------------------------
-UPDATE SITATORT.ELDAVER SET NUMVER='2.7.2.b', DATVET=CURRENT TIMESTAMP WHERE CODAPP='W9';
--- VigilanzaComunicazioni/sitat-webapp/sitatsa-webapp/src/main/config/database/sql/db2/Aggiornamenti/SITATSA_272to272b.sql
+++
@@ -1,9 +0,0 @@
---------------------------------
--- AGGIORNAMENTO SITAT/VIGILANZA
--- Database: DB2
-------------------------------
-
-------------------------------
--- SITATSA 2.7.2.b
-------------------------------
-UPDATE SITATSA.ELDAVER SET NUMVER='2.7.2.b', DATVET=CURRENT TIMESTAMP WHERE CODAPP='W9';
